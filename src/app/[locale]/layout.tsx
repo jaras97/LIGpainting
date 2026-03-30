fd7798e88a6a9a25902ba50a19208f04afb342cb
jaras97 <mateojaras@gmail.com>
2026-03-30 17:20:30 -0400

    fix: update locale handling and ensure correct locale is set in NextIntlClientProvider

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import '../globals.css';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages, setRequestLocale } from 'next-intl/server';
 import { locales } from '@/i18n/routing';
+import { notFound } from 'next/navigation';
 
 export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
@@ -12,11 +13,14 @@ export default async function LocaleLayout({
   params,
 }: {
   children: React.ReactNode;
-  params: Promise<{ locale: 'en' | 'es' }>;
+  params: Promise<{ locale: string }>;
 }) {
   const { locale } = await params;
 
-  // Esto asegura que next-intl use el locale correcto en server components
+  if (!locales.includes(locale as (typeof locales)[number])) {
+    notFound();
+  }
+
   setRequestLocale(locale);
 
   const messages = await getMessages();
@@ -24,7 +28,7 @@ export default async function LocaleLayout({
   return (
     <html lang={locale}>
       <body className='min-h-screen text-neutral-900 antialiased'>
-        <NextIntlClientProvider messages={messages}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
           {children}
         </NextIntlClientProvider>
       </body>
